30c305a4462249dfdaed5f194347a6b6e4808f6d
Anton Pirogov <anton the dot symbol pirogov the at symbol gmail the dot symbol com>
2015-12-13 14:12:43 +0100

    add OverloadedStrings pragma in haddock comment

--- src/SDL/Cairo/Canvas.hs
+++ src/SDL/Cairo/Canvas.hs
@@ -15,31 +15,35 @@ While having the Processing spirit, this module does not aim for a perfect mappi
 deviates where necessary or appropriate. Nevertheless most Processing examples should be
 trivial to port to the Canvas API. Example:
 
->import SDL
->import Linear.V2 (V2(..))
->import SDL.Cairo
->import SDL.Cairo.Canvas
->
->main :: IO ()
->main = do
->  initialize [InitEverything]
->  window <- createWindow "SDL2 Cairo Canvas" defaultWindow
->  renderer <- createRenderer window (-1) defaultRenderer
->  texture <- createCairoTexture' renderer window
->
->  withCanvas texture $ do
->    background $ gray 102
->    fill $ red 255 !@ 128
->    noStroke
->    rect $ D 200 200 100 100
->    stroke $ green 255 !@ 128
->    fill $ blue 255 !@ 128
->    rect $ D 250 250 100 100
->    triangle (V2 400 300) (V2 350 400) (V2 400 400)
->
->  copy renderer texture Nothing Nothing
->  present renderer
->  delay 5000
+@
+\{\-\# LANGUAGE OverloadedStrings \#\-\}
+import SDL
+import Linear.V2 (V2(..))
+import SDL.Cairo
+import SDL.Cairo.Canvas
+
+main :: IO ()
+main = do
+  initialize [InitEverything]
+  window <- createWindow "SDL2 Cairo Canvas" defaultWindow
+  renderer <- createRenderer window (-1) defaultRenderer
+  texture <- createCairoTexture' renderer window
+
+  withCanvas texture $ do
+    background $ gray 102
+    fill $ red 255 !\@ 128
+    noStroke
+    rect $ D 200 200 100 100
+    stroke $ green 255 !\@ 128
+    fill $ blue 255 !\@ 128
+    rect $ D 250 250 100 100
+    triangle (V2 400 300) (V2 350 400) (V2 400 400)
+
+  copy renderer texture Nothing Nothing
+  present renderer
+  delay 5000
+@
+
 -}
 module SDL.Cairo.Canvas (
   -- * Entry point
